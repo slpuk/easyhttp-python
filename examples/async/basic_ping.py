44ffb0f04e7662ad4f2f8cd51959738780ec7a70
slpuk <yarik6052@gmail.com>
2026-02-19 18:53:03 +0300

    refactor: basic_ping.py with context managers

--- examples/async/basic_ping.py
+++ examples/async/basic_ping.py
@@ -1,31 +1,29 @@
+# Simple ping script with EasyHTTPAsync & context manager
 import asyncio
 from easyhttp import EasyHTTPAsync
 
 async def main():
-    # Initializing EasyHTTPAsync
-    easy = EasyHTTPAsync(debug=True, port=5000)
+    # Async context manager whuch initializing EasyHTTPAsync
+    async with EasyHTTPAsync(debug=True, port=5000) as easy:
+        try:
+            # Manually adding other device (Auto-discovery is in progress!)
+            easy.add("ABC123", "192.168.1.100", 5000)
 
-    # Manually adding other device (Auto-discovery is in progress!)
-    easy.add("ABC123", "192.168.1.100", 5000)
+            # Pinging device by ID once every 3 seconds
+            while True:
+                # Bool: True if device is online(sended PONG command)
+                if await easy.ping("ABC123"):
+                    print(f"Device ABC123 is online")
+                else:
+                    print(f"Device ABC123 is offline")
 
-    # Starting API
-    await easy.start()
+                await asyncio.sleep(3)
 
-    # Pinging device by ID once every 3 seconds
-    try:
-        while True:
-            # Bool: True if device is online(sended PONG command)
-            if await easy.ping("ABC123"):
-                print(f"Device ABC123 is online")
-            else:
-                print(f"Device ABC123 is offline")
-
-            await asyncio.sleep(3)
-    except KeyboardInterrupt:
-        # Stopping
-        await easy.stop()
-    except Exception as e:
-        print(e)
+        except KeyboardInterrupt:
+            # Stopping
+            await easy.stop()
+        except Exception as e:
+            print(e)
     
 if __name__ == '__main__':
     asyncio.run(main())
--- examples/sync/basic_ping.py
+++ examples/sync/basic_ping.py
@@ -1,31 +1,29 @@
+# Simple ping script with EasyHTTP & context manager
 import time
 from easyhttp import EasyHTTP
 
 def main():
-    # Initializing EasyHTTP
-    easy = EasyHTTP(debug=True, port=5000)
+    # Async context manager whuch initializing EasyHTTP
+    with EasyHTTP(debug=True, port=5000) as easy:
+        try:
+            # Manually adding other device (Auto-discovery is in progress!)
+            easy.add("ABC123", "192.168.1.100", 5000)
 
-    # Manually adding other device (Auto-discovery is in progress!)
-    easy.add("ABC123", "192.168.1.100", 5000)
+            # Pinging device by ID once every 3 seconds
+            while True:
+                # Bool: True if device is online(sended PONG command)
+                if easy.ping("ABC123"):
+                    print(f"Device ABC123 is online")
+                else:
+                    print(f"Device ABC123 is offline")
 
-    # Starting API
-    easy.start()
-
-    # Pinging device by ID once every 3 seconds
-    try:
-        while True:
-            # Bool: True if device is online(sended PONG command)
-            if easy.ping("ABC123"):
-                print(f"Device ABC123 is online")
-            else:
-                print(f"Device ABC123 is offline")
-
-            time.sleep(3)
-    except KeyboardInterrupt:
-        # Stopping
-        easy.stop()
-    except Exception as e:
-        print(e)
+                time.sleep(3)
+                
+        except KeyboardInterrupt:
+            # Stopping
+            easy.stop()
+        except Exception as e:
+            print(e)
     
 if __name__ == '__main__':
     main()
